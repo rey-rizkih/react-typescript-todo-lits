eca02a825d08d92dd38c0936622d6b66fb44ac17
Rizki Harahap <rizki.harahap@rey.id>
2022-05-23 18:04:01 +0700

    Setup Material UI

---
+++ src/styles/theme.ts
@@ -0,0 +1,48 @@
+import type { BreakpointsOptions, PaletteOptions } from "@mui/material";
+import { createTheme } from "@mui/material";
+import type { TypographyOptions } from "@mui/material/styles/createTypography";
+
+// Breakpoints
+const breakpoints: BreakpointsOptions = {
+  values: {
+    xs: 0,
+    sm: 600,
+    md: 800,
+    lg: 1200,
+    xl: 1536,
+  },
+};
+
+// Palette
+const palette: PaletteOptions = {
+  primary: {
+    main: "#32c3cd",
+  },
+  secondary: {
+    main: "#eb6750",
+  },
+  background: {
+    default: "#2f74c0",
+  },
+};
+
+// Typography
+const typography: TypographyOptions = {
+  fontFamily: ["Neucha", "cursive"].join(),
+  h1: {
+    textTransform: "uppercase",
+    color: "white",
+    fontSize: "40px",
+    margin: "30px 0",
+    "@media (max-width:800px)": {
+      fontSize: "35px",
+      margin: "1.5rem 0",
+    },
+  },
+  h2: {
+    color: "white",
+    fontSize: "30px",
+  },
+};
+
+export const theme = createTheme({ breakpoints, palette, typography });
