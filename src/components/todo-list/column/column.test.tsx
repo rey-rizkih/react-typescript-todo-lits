70e488a1876294ad76139f048913f0b8a7851055
Rizki Harahap <rizki.harahap@rey.id>
2022-06-02 09:50:56 +0700

    Test TodoColumn

--- src/components/todo-list/column/column.test.tsx
+++ src/components/todo-list/column/column.test.tsx
@@ -1,8 +1,6 @@
 import { shallow } from "enzyme";
 import TodoColumn, { TodoColumnProps } from ".";
 
-let wrapper;
-
 const mockTodoColumnProps: TodoColumnProps = {
   column: {
     id: "active",
@@ -11,24 +9,52 @@ const mockTodoColumnProps: TodoColumnProps = {
   },
 };
 
-// jest.mock("react-beautiful-dnd", () => ({
-//   Droppable: (props) => {
-//     return <div data-testid="droppable" {...props} />;
-//   },
-// }));
+jest.mock("react-beautiful-dnd", () => ({
+  Droppable: ({ children }) =>
+    children(
+      {
+        draggableProps: {
+          style: {},
+        },
+        provided: {
+          innerRef: jest.fn(),
+        },
+      },
+      {}
+    ),
+  Draggable: ({ children }) =>
+    children(
+      {
+        draggableProps: {
+          style: {},
+        },
+        innerRef: jest.fn(),
+      },
+      {}
+    ),
+  DragDropContext: ({ children }) => children,
+}));
+
+let wrapper;
 
 describe("<TodoColumn/> rendering", () => {
   beforeEach(() => {
     wrapper = shallow(<TodoColumn {...mockTodoColumnProps} />);
   });
 
-  it("renders correctly", () => {
-    expect(wrapper).toMatchSnapshot();
+  // it("renders correctly", () => {
+  //   expect(wrapper).toMatchSnapshot();
+  // });
+
+  it("should render one", () => {
+    expect(wrapper.find("Droppable")).toHaveLength(1);
   });
 
-  // it("should render one", () => {
-  //   expect(wrapper.find("Droppable").prop("children")()).toHaveLength(1);
-  // });
+  it("should have an children", () => {
+    expect(
+      wrapper.find("Droppable").renderProp("children")()
+    ).toHaveBeenCalled();
+  });
 
   // it("should render <TodoRow/> for each todo", () => {
   //   expect(wrapper.find("TodoRow")).toHaveLength(
